55d36ba6b45a51c9ece660437365a474ec3cc989
Geoiv <george.wood@55places.com>
2020-04-01 16:24:53 -0500

    v0.2.4

--- pipenv_devcheck/__version__.py
+++ pipenv_devcheck/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "pipenv-devcheck"
 __description__ = "Pipenv/setup.py dependency comparison tool"
 __url__ = "https://github.com/neighborhoods/pipenv-devcheck"
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __author__ = "George Wood (@Geoiv)"
 __author_email__ = "george.wood@55places.com"
 __license__ = "MIT"
