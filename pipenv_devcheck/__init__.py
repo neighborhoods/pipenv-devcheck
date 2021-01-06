cfba5778c856a00277c73ac28234f3868234e6c3
George Wood <george.wood@55places.com>
2021-01-05 19:19:40 -0600

    flake8

--- pipenv_devcheck/__init__.py
+++ pipenv_devcheck/__init__.py
@@ -1,6 +1,6 @@
 from .pipenv_setup_comp import compare_deps
 from ._version import (__title__, __description__, __url__, __version__,
-                          __author__, __author_email__, __license__)
+                       __author__, __author_email__, __license__)
 
 __all__ = ["compare_deps", "__title__", "__description__", "__url__",
            "__version__", "__author__", "__author_email__", "__license__"]
